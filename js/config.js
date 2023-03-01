9440cf02b86f3380e643a525ffeac94d5756079d
Miles123K <yh643@georgetown.edu>
2023-02-28 20:12:26 -0500

    new config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_oLLYhEVWJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3omtvq2mcbhmjabjse69f0q1s5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_YomCBU9dr', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1k0ojdf9lhs9hg9i7467r2s5cd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
